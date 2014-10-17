1d10dc5a5e38fca62fda7ceca1dab2e1642189ef
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-10-17 10:14:40 +0000

    taking a picture and showing it

---
+++ Verus App/app/buisnessLogic/imageOperations.js
@@ -0,0 +1,13 @@
+define([], function () {
+
+    var io = {
+        mimeMap: {
+            jpg: "image/jpeg",
+            jpeg: "image/jpeg",
+            png: "image/png",
+            gif: "image/gif"
+        }
+    };
+
+    return io;
+});
--- Verus App/app/views/newStory/newStory.js
+++ Verus App/app/views/newStory/newStory.js
@@ -1,8 +1,50 @@
 define([
     'views/view',
     'text!views/newStory/newStory.html',
-    'app'
-], function (View, html, app) {
+    'app',
+    'buisnessLogic/imageOperations'
+], function (View, html, app, imageOperations) {
     
-    return new View('newStory', html, null, null);
+    var tookPicture = false;
+    
+    var events = {
+        init: function (e) {
+
+        },
+        afterShow: function (e) {
+            var self = this;
+
+
+        }
+    };
+
+    var model = kendo.observable({
+        addImage: function (e) {
+            if(tookPicture == true)
+            {
+                return;
+            }
+            var story = e.data;
+
+            var success = function (data) {
+                var imageElement = '<img src="data:image/jpg;charset=utf-8;base64,' + data + 
+                    '" class="photo-image"/>';
+                var tab = $('#newStoryPicture');
+                tab.append(imageElement);
+                tookPicture = true;
+            };
+
+            var error = function () {
+                navigator.notification.alert("Unfortunately we could not add the image");
+            };
+
+            var config = {
+                destinationType: Camera.DestinationType.DATA_URL
+            }
+
+            navigator.camera.getPicture(success, error, config)
+        }
+    });
+
+    return new View('newStory', html, model, events);
 });
